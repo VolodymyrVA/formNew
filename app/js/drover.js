4de3e57275fa6ea221c4dafdfd7d222104b0e3a3
VolodymyrVA <vysotskyi.volodymyr84@gmail.com>
2017-03-29 00:08:04 +0300

    drover createElement

--- app/js/drover.js
+++ app/js/drover.js
@@ -11,23 +11,30 @@ class Drover {
     }
 
     createElement(itemDescription) {
-        let elem;
-        for(let key in itemDescription) {
-            if (key == 'element') elem = document.createElement(itemDescription[key]);
-            if (key == 'class') elem.className = itemDescription[key];
-            if (key == 'attr') {
-                for (let keyAttr in itemDescription[key]) {
-                    elem.setAttribute(keyAttr, itemDescription[key][keyAttr]);
+        let elem = document.createElement(itemDescription.element),
+            descriptionMap = {
+                classes: (classes) => elem.className = classes,
+                content: (content) => elem.innerHTML =content,
+                attr: (attrMap) => {
+                    for(let attribut in attrMap) {
+                        elem.setAttribute(attribut, attrMap[attribut])
+                    }
                 }
+            };
+
+        for(let item in itemDescription){
+            if(item != 'element'){
+                let funktion = descriptionMap[item];
+                funktion(itemDescription[item]);
             }
-            if (key == 'content') elem.innerHTML = itemDescription[key];
+
         }
         return elem;
     }
 
     createWrapper() {
-        this.divWrapper = this.createElement({element: 'div', class: 'divWrapper hidden'});
-        this.title = this.createElement({element: 'div', class: 'form-title'});
+        this.divWrapper = this.createElement({element: 'div', classes: 'divWrapper hidden'});
+        this.title = this.createElement({element: 'div', classes: 'form-title'});
         let p = this.createElement({element: 'p', content: 'Login Form'});
         this.title.appendChild(p);
         this.titleClose = this.createElement({element: 'span', content: '&#215'});
@@ -37,20 +44,20 @@ class Drover {
 
     createInputForm(filde){
         for (let key in filde) {
-            let input = this.createElement({element:'input', class:'input-form',attr: {type: key.type, placeholder: key, name: key}});
+            let input = this.createElement({element:'input', classes:'input-form',attr: {type: key.type, placeholder: key, name: key}});
             this.form.appendChild(input);
         }
     }
 
     createForm(filde) {
-        this.form = this.createElement({element:'form', class: 'log-form', attr: {action: '#'}});
+        this.form = this.createElement({element:'form', classes: 'log-form', attr: {action: '#'}});
         this.createInputForm(filde);
-        this.formButton = this.createElement({element:'button', class:'submit-button', attr: {
+        this.formButton = this.createElement({element:'button', classes:'submit-button', attr: {
             type: 'submit',
             value: 'submit',
         }, content:'Sing In'});
         this.form.appendChild(this.formButton);
-        let batton = this.createElement({element:'button', class: 'lost-password', content: 'Lost Your Password ?'});
+        let batton = this.createElement({element:'button', classes: 'lost-password', content: 'Lost Your Password ?'});
         this.form.appendChild(batton);
         return this.form;
     }
